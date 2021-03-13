3afde2fad1c50216009766946667443b8615c013
Jangbe <jangebe91@gmail.com>
2021-03-13 10:01:43 +0700

    tertinggal boss

--- app/controllers/Operator.php
+++ app/controllers/Operator.php
@@ -1,15 +1,15 @@
 <?php
 
-class Nasabah extends Controller{
+class Operator extends Controller{
     public function __construct()
     {
-        $this->middleware(['admin', 'operator', 'nasabah']);
+        $this->middleware(['admin', 'operator']);
     }
 
     public function index()
     {
         $this->view('layouts/header');
-        $this->view('nasabah/index');
+        $this->view('pegawai/index');
         $this->view('layouts/footer');
     }
 }
